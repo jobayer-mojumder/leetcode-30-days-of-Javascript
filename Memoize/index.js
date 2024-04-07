3bbf62d1953d21b05f90e96c527a3b8958a33b3e
Jobayer Mojumder <jobayer.mojumder@burda-forward.de>
2024-04-07 04:02:22 +0200

    solution for Memoize

--- Memoize/index.js
+++ Memoize/index.js
@@ -16,14 +16,14 @@ function memoize(fn) {
 
 
 
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) {
-    callCount += 1;
-    return a + b;
-})
-memoizedFn(2, 3) // 5
-console.log(callCount)
-memoizedFn(2, 3) // 5
-console.log(callCount) // 1
-memoizedFn(2, 3) // 5
-console.log(callCount) // 1 
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//     callCount += 1;
+//     return a + b;
+// })
+// memoizedFn(2, 3) // 5
+// console.log(callCount)
+// memoizedFn(2, 3) // 5
+// console.log(callCount) // 1
+// memoizedFn(2, 3) // 5
+// console.log(callCount) // 1 
